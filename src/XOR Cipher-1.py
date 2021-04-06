baa79a9edc1a483d631e5d99ca1a6d5a3baf8aea
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-06 11:39:59 +0900

    Create XOR Cipher-1.py

---
+++ src/XOR Cipher-1.py
@@ -0,0 +1,13 @@
+#Kinesys XOR Cipher -1.py
+
+
+_encode = '암호내용' .decode('hex')
+
+text = ' '
+
+for x in _encode:
+         m = l.decode('hex')
+         m = int(m, 16) ^ 79
+         text += chr(m)
+         
+ print text
